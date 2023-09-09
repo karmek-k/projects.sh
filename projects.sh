7a2a60f89bef8bc01483b9d87a3c32e6b6f1cf53
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-09-09 18:07:42 +0200

    Fix shellcheck errors

--- projects.sh
+++ projects.sh
@@ -11,8 +11,7 @@ run_project() {
     PROJECT_NAME="$1"
 
     # check if the code editor executable file is empty
-    echo "$MY_EDITOR" | grep '\S' > /dev/null
-    if [ "$?" != 0 ]; then
+    if ! (echo "$MY_EDITOR" | grep '\S' > /dev/null); then
         die "I don't know how to run your favorite code editor. 
 Please set the \`MY_EDITOR\` env variable
 or edit this script and set a command for editing"
@@ -26,7 +25,7 @@ or edit this script and set a command for editing"
         return 1
     fi
 
-    $MY_EDITOR $PROJECT_PATH
+    $MY_EDITOR "$PROJECT_PATH"
 }
 
 # list project directory if no project provided
@@ -36,8 +35,8 @@ if [ -z "$PROJECT" ]; then
         exit 1
     fi
 
-    ls -1 $MY_PROJECT_DIR
+    ls -1 "$MY_PROJECT_DIR"
     exit
 fi
 
-run_project $PROJECT
+run_project "$PROJECT"
